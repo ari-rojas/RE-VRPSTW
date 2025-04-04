85bb8934bf3d56307155a19ee71bd9037f9f4c5a
Ariel Rojas <ari_rojasa@hotmail.com>
2025-04-04 10:54:22 +0200

    First

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -241,7 +241,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "Lexicographic", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "Lxcgraphic2", args[2]);
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
 	}
