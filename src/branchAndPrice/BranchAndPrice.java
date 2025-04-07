c0782c5c1cfd5e13de7d3d2a7ed61a47e3a5e750
Ariel Rojas <ari_rojasa@hotmail.com>
2025-04-07 10:02:25 +0200

    Lexi2

--- src/branchAndPrice/BranchAndPrice.java
+++ src/branchAndPrice/BranchAndPrice.java
@@ -313,17 +313,14 @@ public final class BranchAndPrice extends AbstractBranchAndPrice<EVRPTW,Route,Pr
 
 							} else {
 								
-								foundBranches = bc.canPerformFirstBranching(bapNode.getSolution());
+								bapNode.storeSolution(new_cost, timeLimit, old_solution, bapNode.getInequalities());
+								
+								foundBranches = bc.canPerformBranching(bapNode.getSolution());
 								if (foundBranches){
 									this.notifier.fireNodeIsFractionalEvent(bapNode, bapNode.getBound(), bapNode.getObjective());
-									newBranches.addAll(bc.getFirstBranches(bapNode));
-								} else {
-									foundBranches = bc.canPerformBranching(bapNode.getSolution());
-									if (foundBranches){
-										this.notifier.fireNodeIsFractionalEvent(bapNode, bapNode.getBound(), bapNode.getObjective());
-										newBranches.addAll(bc.getBranches(bapNode));
-									}
+									newBranches.addAll(bc.getBranches(bapNode));
 								}
+								
 								
 							}
 
