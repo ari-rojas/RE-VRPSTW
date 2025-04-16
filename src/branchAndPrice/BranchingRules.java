afba29e0eb0c247dc9d1f5768e06e9c482c42340
Ariel Rojas <ari_rojasa@hotmail.com>
2025-04-16 13:04:41 +0200

    LexicoMinMax - lR

--- src/branchAndPrice/BranchingRules.java
+++ src/branchAndPrice/BranchingRules.java
@@ -187,6 +187,7 @@ public final class BranchingRules extends AbstractBranchCreator<EVRPTW, Route, P
 		BAPNode<EVRPTW,Route> node1; 		//other child node
 		
 		if(branchOnInitialChargingTime) {
+			// DIVING LEFT
 			//Branch 1: remove the edge:
 			BranchInitialChargingTimeDown branchingDecision1= new BranchInitialChargingTimeDown(this.pricingProblems.get(0), (int) Math.floor(bestTimestepValue),parentNode.getInequalities(), this.timestepForBranching);
 			node2=this.createBranch(parentNode, branchingDecision1, parentNode.getInitialColumns(), parentNode.getInequalities());
@@ -194,14 +195,14 @@ public final class BranchingRules extends AbstractBranchCreator<EVRPTW, Route, P
 			BranchInitialChargingTimeUp branchingDecision2=new BranchInitialChargingTimeUp(this.pricingProblems.get(0), (int) Math.ceil(bestTimestepValue),parentNode.getInequalities(), this.timestepForBranching);
 			node1=this.createBranch(parentNode, branchingDecision2, parentNode.getInitialColumns(), parentNode.getInequalities());
 		} else {
-			//CURRENTLY DIVING TO THE LEFT (REMOVING - L)
+			//CURRENTLY DIVING TO THE RIGHT (FIXING - R)
 
 			//Branch 1: remove the edge:
 			BranchEndChargingTimeDown branchingDecision1= new BranchEndChargingTimeDown(this.pricingProblems.get(0), (int) Math.floor(bestTimestepValue),parentNode.getInequalities(), this.timestepForBranching);
-			node2=this.createBranch(parentNode, branchingDecision1, parentNode.getInitialColumns(), parentNode.getInequalities());
+			node1=this.createBranch(parentNode, branchingDecision1, parentNode.getInitialColumns(), parentNode.getInequalities());
 			//Branch 2: fix the edge:
 			BranchEndChargingTimeUp branchingDecision2=new BranchEndChargingTimeUp(this.pricingProblems.get(0), (int) Math.ceil(bestTimestepValue),parentNode.getInequalities(), this.timestepForBranching);
-			node1=this.createBranch(parentNode, branchingDecision2, parentNode.getInitialColumns(), parentNode.getInequalities());
+			node2=this.createBranch(parentNode, branchingDecision2, parentNode.getInitialColumns(), parentNode.getInequalities());
 		}
 		
 		return Arrays.asList(node1,node2);
