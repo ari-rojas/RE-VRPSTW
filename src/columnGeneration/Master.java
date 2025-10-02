d5d33f32da94cc322f882bafea511b5e22f1425b
Ariel Rojas <ari_rojasa@hotmail.com>
2025-10-02 12:25:50 -0400

    Barrier Config

--- src/columnGeneration/Master.java
+++ src/columnGeneration/Master.java
@@ -71,8 +71,9 @@ public final class Master extends AbstractMaster<EVRPTW, Route, PricingProblem, 
 			cplex.setOut(null); 			//disable CPLEX output
 			//			System.out.println(cplex.getVersion());
 			cplex.setParam(IloCplex.Param.RootAlgorithm, IloCplex.Algorithm.Barrier); //Primal Simplex
-			cplex.setParam(IloCplex.Param.Simplex.Tolerances.Feasibility, 1e-9);
-			cplex.setParam(IloCplex.Param.Simplex.Tolerances.Optimality, 1e-6);
+			/* cplex.setParam(IloCplex.Param.Simplex.Tolerances.Feasibility, 1e-9);
+			cplex.setParam(IloCplex.Param.Simplex.Tolerances.Optimality, 1e-6); */
+			cplex.setParam(IloCplex.Param.Barrier.ConvergeTol, 1e-5);
 			cplex.setParam(IloCplex.Param.RandomSeed, 30);
 
 			obj= cplex.addMinimize();		//objective
@@ -710,7 +711,8 @@ public final class Master extends AbstractMaster<EVRPTW, Route, PricingProblem, 
 			
 			//masterData.cplex.exportModel("./results/log/"+dataModel.algorithm+"/"+dataModel.experiment+"/model.lp");
 			masterData.cplex.setParam(IloCplex.Param.RootAlgorithm, IloCplex.Algorithm.Barrier);
-			masterData.cplex.setParam(IloCplex.Param.Simplex.Tolerances.Feasibility, 1e-9);
+			masterData.cplex.setParam(IloCplex.Param.Barrier.ConvergeTol, 1e-5);
+			//masterData.cplex.setParam(IloCplex.Param.Simplex.Tolerances.Feasibility, 1e-9);
 			masterData.cplex.setParam(IloCplex.Param.Read.Scale, -1);  // disable scaling
 			this.masterData.optimal = this.solveMasterProblem(timeLimit);
 			new_cost = masterData.cplex.getValue(expr);
