186b0986e152957aae1f68079a7d447b4cd0f24a
Ariel Rojas <ari_rojasa@hotmail.com>
2025-10-20 15:59:01 -0400

    s

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -245,7 +245,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "New Pricing Acc", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "New Pricing", args[2]);
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
 	}
