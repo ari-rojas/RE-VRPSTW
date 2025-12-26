f929e5830ac4784eb14258e276f5c41e5b34a899
Ariel Rojas <ari_rojasa@hotmail.com>
2025-12-26 16:29:00 -0500

    Name

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -239,7 +239,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "RE-VRSPTW", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "Original", args[2]);
 		//EVRPTW evrptw = new EVRPTW("C204-25", 1, 0, true, "RE-VRSPTW", "Debug");
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
