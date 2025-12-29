d86da568ccb5ffe912e5c091b6984422f6090498
Ariel Rojas <ari_rojasa@hotmail.com>
2025-12-29 11:00:13 -0500

    Data

--- src/model/EVRPTW.java
+++ src/model/EVRPTW.java
@@ -58,7 +58,7 @@ public final class EVRPTW implements ModelInterface {
 	public long heuristicPricingTime = 0; 					//time spent on the heuristic labeling algorithm
 	public int columnsRootNode = 0; 						//columns generated at the root node
 	public int cutsRootNode = 0; 							//cuts separated at the root node
-	public int[] infeasibleArcs; 							//infeasible arcs in the pricing problem
+	public int[] infeasibleArcs; 						//infeasible arcs in the pricing problem
 
 	//Log file creation
 	public boolean print_log;
@@ -259,27 +259,27 @@ public final class EVRPTW implements ModelInterface {
 			this.numArcs++;
 			Node link = linkNodes.item(i);
 			Element linkElement = (Element) link;
-			
-			Element customElements = (Element) linkElement.getElementsByTagName("custom").item(0);
-			
+
 			//id, head and tail
 			int id = Integer.parseInt(linkElement.getAttribute("id"));
-			//int id = this.numArcs;
 			int tail = Integer.parseInt(linkElement.getAttribute("tail"));
 			int head = Integer.parseInt(linkElement.getAttribute("head"));
-			
+
 			//travel cost and travel time
 			int cost = Integer.parseInt(linkElement.getElementsByTagName("travel_cost").item(0).getTextContent());
 			int time = Integer.parseInt(linkElement.getElementsByTagName("travel_time").item(0).getTextContent());
-			
+
 			//custom elements (energy and minimum values)
+			Element customElements = (Element) linkElement.getElementsByTagName("custom").item(0);
 			int energy = Integer.parseInt(customElements.getElementsByTagName("energy_consumption").item(0).getTextContent());
 			int energy_deviation = 0;
 			if (!this.getName().substring(0, 2).equals("DY")) energy_deviation = Integer.parseInt(customElements.getElementsByTagName("energy_deviation").item(0).getTextContent());
-			
-			Arc newArc = new Arc(id, tail, head, cost, time, energy, energy_deviation);
+			int min_energy = Integer.parseInt(customElements.getElementsByTagName("min_energy").item(0).getTextContent());
+
+			Arc newArc = new Arc(id, tail, head, cost, time, energy, energy_deviation, min_energy);
 			arcs[id] = newArc;
 			graph.addEdge(tail, head, newArc);
+
 			
 		}
 	}
@@ -393,13 +393,14 @@ public final class EVRPTW implements ModelInterface {
 		public int time; 							//time of the arc
 		public int energy; 							//energy of the arc
 		public int energy_deviation;				//worst-case energy deviation of the arc
+		public int min_energy;
 		public double modifiedCost; 				//modified cost of the arc
 
 		/**
 		 * Creates a new arc.
 		 * @throws IOException Throws IO exception when the instance cannot be found.
 		 */
-		public Arc(int id, int tail, int head, int cost, int time, int energy, int energy_deviation) {
+		public Arc(int id, int tail, int head, int cost, int time, int energy, int energy_deviation, int min_energy) {
 			this.id = id;
 			this.tail = tail;
 			this.head = head;
@@ -407,6 +408,7 @@ public final class EVRPTW implements ModelInterface {
 			this.time = time;
 			this.energy = energy;
 			this.energy_deviation = energy_deviation;
+			this.min_energy = min_energy;
 			this.modifiedCost = 0.0;
 		}
 
