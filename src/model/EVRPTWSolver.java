7d5db2e46df8cf193c6bc5f1fb64931e72952c9f
Ariel Rojas <ari_rojasa@hotmail.com>
2026-01-01 20:08:17 -0500

    Fix312

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -242,7 +242,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "FRC", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "FRC312", args[2]);
 		//EVRPTW evrptw = new EVRPTW("C204-25", 5, 0, true, "FRC", "Debug");
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
