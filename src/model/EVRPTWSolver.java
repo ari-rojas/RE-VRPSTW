168dbc2397e204ba3b27bacb4250429b7d223a5c
Ariel Rojas <ari_rojasa@hotmail.com>
2026-01-03 16:21:46 -0500

    325

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -54,7 +54,7 @@ import columnGeneration.HeuristicMinCostLabelingPricingProblemSolver;
 import columnGeneration.HeuristicLabelingPricingProblemSolver;
 import columnGeneration.HeuristicLabelingSecondPricingProblemSolver;
 import columnGeneration.HeuristicLabelingThirdPricingProblemSolver;
-import columnGeneration.HeuristicLabelingFourthPricingProblemSolver;
+import columnGeneration.HeuristicLabelingFifthPricingProblemSolver;
 import columnGeneration.Master;
 import columnGeneration.PricingProblem;
 import columnGeneration.Route;
@@ -94,8 +94,9 @@ public final class EVRPTWSolver {
 		//Define which solvers to use (one or more)
 		List<Class<? extends AbstractPricingProblemSolver<EVRPTW, Route, PricingProblem>>> solvers = new ArrayList<>(); // The solvers list of classes is restricted to subclasses of AbstractPricingProblemSolver with the specified parameters
 		solvers.add(HeuristicLabelingThirdPricingProblemSolver.class); // Adding the classes themselves, not instances of them.
-		solvers.add(HeuristicLabelingPricingProblemSolver.class);
+		//solvers.add(HeuristicLabelingPricingProblemSolver.class);
 		solvers.add(HeuristicLabelingSecondPricingProblemSolver.class);
+		solvers.add(HeuristicLabelingFifthPricingProblemSolver.class);
 		solvers.add(HeuristicMinCostLabelingPricingProblemSolver.class);
 		
 		//Create a set of initial columns and use it as an upper bound
@@ -242,7 +243,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "FRCMono312", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "FRCMono325", args[2]);
 		//EVRPTW evrptw = new EVRPTW("R204-25", 6, 0, true, "FRC312", "Debug");
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
