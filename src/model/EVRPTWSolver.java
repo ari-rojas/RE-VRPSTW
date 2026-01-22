bf96b9824afe9d3ff92017d3ed060376fe32af8f
Ariel Rojas <ari_rojasa@hotmail.com>
2026-01-22 11:25:38 -0500

    Max cols per iteration

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -252,7 +252,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "NewPricing2", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "NewPricing2-400-DY", args[2]);
 		//EVRPTW evrptw = new EVRPTW("C101-25", 0, 0, true, "Priority312", "Debug");
 		EVRPTWSolver Solver = new EVRPTWSolver(evrptw, null);
 
