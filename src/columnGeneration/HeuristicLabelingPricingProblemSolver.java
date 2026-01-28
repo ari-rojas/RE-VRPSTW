441bc2d2694637524e2579c6b7e1248b5fcbe433
Ariel Rojas <ari_rojasa@hotmail.com>
2026-01-28 09:12:08 -0500

    Dominance at the depot

--- src/columnGeneration/HeuristicLabelingPricingProblemSolver.java
+++ src/columnGeneration/HeuristicLabelingPricingProblemSolver.java
@@ -53,8 +53,12 @@ public final class HeuristicLabelingPricingProblemSolver extends AbstractPricing
 		while (!nodesToProcess.isEmpty() && System.currentTimeMillis()<timeLimit) {
 			ArrayList<Label> labelsToProcessNext = labelsToProcessNext();
 			for(Label currentLabel: labelsToProcessNext) {
-				boolean isDominated = checkDominance(currentLabel);
+				
+				boolean isDominated;
+				if (currentLabel.vertex == 0) isDominated = checkDominanceAtDepot(currentLabel);
+				else isDominated = checkDominance(currentLabel);
 				if(isDominated) continue;
+				
 				else if (currentLabel.vertex > 0) { // The non-dominated label is marked as processed and extended along its incoming arcs.
 					currentLabel.index = vertices[currentLabel.vertex].processedLabels.size();
 					vertices[currentLabel.vertex].processedLabels.add(currentLabel);
@@ -70,7 +74,7 @@ public final class HeuristicLabelingPricingProblemSolver extends AbstractPricing
 				} else { // The currentVertex is 0, the label corresponds to a complete route.
 					
 					List<Label> labels_to_remove = new ArrayList<>();
-					for (Label processedLabel: vertices[0].processedLabels){ if (isDominated(processedLabel, currentLabel)) labels_to_remove.add(processedLabel); }
+					for (Label processedLabel: vertices[0].processedLabels){ if (isDominatedAtDepot(processedLabel, currentLabel)) labels_to_remove.add(processedLabel); }
 					vertices[0].processedLabels.removeAll(labels_to_remove);
 
 					vertices[0].processedLabels.add(currentLabel);
@@ -121,7 +125,8 @@ public final class HeuristicLabelingPricingProblemSolver extends AbstractPricing
 			else {
 				boolean isDominated = false;
 				for(Label L2: labelsToProcessNext) {
-					isDominated = isDominated(currentLabel, L2);
+					if (currentVertex.id == 0) isDominated = isDominatedAtDepot(currentLabel, L2);
+					else isDominated = isDominated(currentLabel, L2);
 					if(isDominated) break;
 				}
 				if(!isDominated) labelsToProcessNext.add(currentLabel);
@@ -410,6 +415,35 @@ public final class HeuristicLabelingPricingProblemSolver extends AbstractPricing
 	}
 
 	/**
+	 * Verifies if a label is dominated. Returns true if it is, false otherwise.
+	 * If the label is dominated it is discarded
+	 * If the label is not dominated, the existing labels dominated by the label is discarded
+	 * @param label to which check dominance
+	 */
+	public boolean checkDominanceAtDepot(Label newLabel) {
+
+		Vertex currentVertex = vertices[newLabel.vertex];
+		ArrayList<Label> labelsToDelete = new ArrayList<Label>();
+		for(Label existingLabel: currentVertex.unprocessedLabels) {
+
+			if(isDominatedAtDepot(existingLabel, newLabel)) {
+				//existing_is_discarded = true; // DELETE LATER
+				labelsToDelete.add(existingLabel);
+			}
+			
+		}
+		currentVertex.unprocessedLabels.removeAll(labelsToDelete);
+		if(currentVertex.unprocessedLabels.isEmpty()) nodesToProcess.remove(currentVertex);
+
+		for(Label existingLabel: currentVertex.processedLabels) {
+			if(isDominatedAtDepot(newLabel, existingLabel)) return true;
+			
+		}
+
+		return false;
+	}
+
+	/**
 	 * Verifies if L1 is (strongly) dominated by L2 (Heuristic rule)
 	 * @param L1, L2 labels
 	 */
@@ -446,6 +480,20 @@ public final class HeuristicLabelingPricingProblemSolver extends AbstractPricing
 			if (L2.reducedCost-reducedCostL2-L1.reducedCost>dataModel.precision) return false;
 			return true;
 		}
+	}
+
+	/**
+	 * Verifies if L1 is (strongly) dominated by L2 (Heuristic rule)
+	 * @param L1, L2 labels
+	 */
+	public boolean isDominatedAtDepot(Label L1, Label L2) {
+
+		if (L1.vertex>0 && L1.remainingLoad > L2.remainingLoad) return false; 		// Load
+		if ((int)(L1.remainingTime/10) > (int)(L2.remainingTime/10)) return false; 	// Departure Time
+		if (L1.chargingTime < L2.chargingTime) return false;						// Charging Time
+		if (L2.reducedCost > L1.reducedCost + dataModel.precision) return false;	// Reduced Cost
+		
+		return true;
 	}
 
 	/**
