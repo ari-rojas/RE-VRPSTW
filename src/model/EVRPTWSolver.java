56e7a1f8be9c77e5157eb278e17898e8cca2fd0e
Ariel Rojas <ari_rojasa@hotmail.com>
2026-01-28 08:54:13 -0500

    800

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -252,7 +252,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "NewPricing2Pro", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "NewPricing2Pro-800", args[2]);
 		//EVRPTW evrptw = new EVRPTW("R207-50", 4, 0, true, "NewPricing2", "Debug");
 		EVRPTWSolver Solver = new EVRPTWSolver(evrptw, null);
 
