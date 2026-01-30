3a83e7d5f6bc6d1b4bbb2e54b8a20f502d9c0ff7
Ariel Rojas <ari_rojasa@hotmail.com>
2026-01-30 15:12:03 -0500

    Root Acceleration

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -239,8 +239,8 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "OriginalClean-Bound", args[2]);
-		//EVRPTW evrptw = new EVRPTW("R202-50", 9, 0, true, "OriginalClean-Bound", "Debug");
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "OriginalClean-Root", args[2]);
+		//EVRPTW evrptw = new EVRPTW("C108-50", 1, 0, true, "OriginalClean-Bound", "Debug");
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
 	}
