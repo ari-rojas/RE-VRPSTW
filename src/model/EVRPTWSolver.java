b6aee94f6b91c66bdf30d469e604e3de8475709c
Ariel Rojas <ari_rojasa@hotmail.com>
2026-02-02 11:33:23 -0500

    a

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -240,7 +240,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "ExNewPricing-RootBound", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "ExNewPricing-Bound", args[2]);
 		//EVRPTW evrptw = new EVRPTW("R202-50", 9, 0, true, "NewPricing-Bound", "Debug");
 		EVRPTWSolver Solver = new EVRPTWSolver(evrptw, null);
 
