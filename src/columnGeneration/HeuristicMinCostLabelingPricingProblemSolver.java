b24c987e91a52da2d457cebf572dd5170051b2a8
Ariel Rojas <ari_rojasa@hotmail.com>
2026-02-08 10:55:30 -0500

    oops

--- src/columnGeneration/HeuristicMinCostLabelingPricingProblemSolver.java
+++ src/columnGeneration/HeuristicMinCostLabelingPricingProblemSolver.java
@@ -27,7 +27,7 @@ public final class HeuristicMinCostLabelingPricingProblemSolver extends Abstract
 
 	public Vertex[] vertices = dataModel.vertices; 			//vertices of the instance
 	public PriorityQueue<Vertex> nodesToProcess; 			//labels that need be processed
-	public final int numCols = 400; 						//maximum number of routes (columns) allowed
+	public final int numCols = 800; 						//maximum number of routes (columns) allowed
 	public int[] infeasibleArcs; 						//arcs that cannot be used by branching
 	public final int similarityThreshold = 5; 				//diversification of columns
 
@@ -99,7 +99,7 @@ public final class HeuristicMinCostLabelingPricingProblemSolver extends Abstract
 		}
 
 		//Labeling algorithm 
-		while (!nodesToProcess.isEmpty() && vertices[0].unprocessedLabels.size() <= numCols && System.currentTimeMillis()<timeLimit) {
+		while (!nodesToProcess.isEmpty() && vertices[dataModel.V].unprocessedLabels.size() <= numCols && System.currentTimeMillis()<timeLimit) {
 			ArrayList<Label> labelsToProcessNext = labelsToProcessNext();
 			for(Label currentLabel: labelsToProcessNext) {
 				
