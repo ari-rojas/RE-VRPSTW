7c8db7c7685586880d47f594fa3559ac49a4e1f1
Ariel Rojas <ari_rojasa@hotmail.com>
2026-03-09 13:03:57 +0100

    Dominance acceleration

--- src/columnGeneration/HeuristicLabelingPricingProblemSolver.java
+++ src/columnGeneration/HeuristicLabelingPricingProblemSolver.java
@@ -378,9 +378,7 @@ public final class HeuristicLabelingPricingProblemSolver extends AbstractPricing
 			if (L1.vertex>0 && L2.remainingLoad<L1.remainingLoad) return false; //load
 			if (L2.remainingTime<L1.remainingTime) return false; 				//time
 
-			for (int gam = 0; gam <= dataModel.gamma; gam ++){
-				if (L2.remainingEnergy[gam]<L1.remainingEnergy[gam]) return false;				 //energy
-			}
+			if (L2.remainingEnergy[dataModel.gamma]<L1.remainingEnergy[dataModel.gamma]) return false;	//energy
 			
 			//reducedCost
 			double reducedCostL2 = 0;
