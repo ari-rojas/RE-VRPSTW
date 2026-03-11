b8732e90acc5fbb3692e8ad831ae50d47cbbfbe2
Ariel Rojas <ari_rojasa@hotmail.com>
2026-03-11 10:40:31 +0100

    name

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -234,7 +234,7 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "TwoLabeling-CS", args[2]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "TwoLabeling-Bound-CS", args[2]);
 		//EVRPTW evrptw = new EVRPTW("C108-50", 1, 0, true, "TwoLabeling-Bound", "Debug");
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
