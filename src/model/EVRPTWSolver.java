f12fc89726e9d3468f68ddfcb6d6599df29f82e6
Ariel Rojas <ari_rojasa@hotmail.com>
2026-04-13 18:20:17 +0200

    IP

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -238,8 +238,8 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "BL-CS-Bound-Root", args[2]);
-		//EVRPTW evrptw = new EVRPTW("R201-50", 3, 0, true, "OriginalClean-Bound", "Debug");
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "BL-IP-CS-Bound-Root", args[2]);
+		//EVRPTW evrptw = new EVRPTW("R205-25", 3, 0, true, "BL", "Debug");
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
 	}
