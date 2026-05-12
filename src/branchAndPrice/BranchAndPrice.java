47ab90eec143613dc53de1bba62bb1b0b1348302
Ariel Rojas <ari_rojasa@hotmail.com>
2026-05-12 15:24:28 +0200

    No MIP for now

--- src/branchAndPrice/BranchAndPrice.java
+++ src/branchAndPrice/BranchAndPrice.java
@@ -507,8 +507,8 @@ public final class BranchAndPrice extends AbstractBranchAndPrice<EVRPTW,Route,Pr
 							
 							time = System.currentTimeMillis();
 
-							foundBranches = this.findIntegerSolution(bapNode);
-							//foundBranches = false;
+							//foundBranches = this.findIntegerSolution(bapNode);
+							foundBranches = false;
 
 							if (foundBranches){
 								
