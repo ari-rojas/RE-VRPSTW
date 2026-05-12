a1ce44d1d2524617dba26715f510fec2609a9ac8
Ariel Rojas <ari_rojasa@hotmail.com>
2026-05-12 15:25:50 +0200

    Name

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -241,7 +241,7 @@ public final class EVRPTWSolver {
 		String energy_deviation = args[3];
 		if (!energy_deviation.equals("")) energy_deviation = "-"+energy_deviation;
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "NF"+energy_deviation, args[2], args[3]);
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "NF-IP-Bound-Root"+energy_deviation, args[2], args[3]);
 		//EVRPTW evrptw = new EVRPTW("R106-25", 10, 0, true, "NF", "Debug", "");
 		EVRPTWSolver Solver = new EVRPTWSolver(evrptw, new ArrayList<>());
 
