57cc954343490de34857dbc50f1727549f3f934b
Ariel Rojas <ari_rojasa@hotmail.com>
2026-05-18 20:14:52 +0200

    FRC

--- src/columnGeneration/customCG.java
+++ src/columnGeneration/customCG.java
@@ -309,13 +309,13 @@ public class customCG extends ColGen<EVRPTW, Route, PricingProblem> {
 					extendedNotifier.fireFinishIPSolutionEvent(System.currentTimeMillis() - IPtime);
 				
 				}
-
-				if (!masterSolutionIsInteger &&  (1-this.boundOnMasterObjective/this.cutoffValue) <= 0.05){
-					perform_fixing_by_reduced_cost(timeLimit);}
-
+				
 				// If the IP found a better integer solution, the gap reduction is computed using the newly updated Upper Bound
 				this.gapReduction = (master.getObjective()-this.boundOnMasterObjective)/(this.cutoffValue-this.boundOnMasterObjective);
 				this.boundOnMasterObjective = master.getObjective(); // Update the Bound before adding cuts
+				
+				if (!masterSolutionIsInteger &&  (1-this.boundOnMasterObjective/this.cutoffValue) <= 0.05){
+					perform_fixing_by_reduced_cost(timeLimit);}
 			}
 			
 
