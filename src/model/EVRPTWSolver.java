6f657c5f617b4ee17e1624cf78a919ecf042e3a0
Ariel Rojas <ari_rojasa@hotmail.com>
2026-05-28 10:17:13 +0200

    update

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -41,8 +41,12 @@ import branchAndPrice.BranchingRules;
 import branchAndPrice.CGMasterIsInfeasibleEvent;
 import branchAndPrice.CGProblemsLBEvent;
 import branchAndPrice.ExtendBAPListener;
+import branchAndPrice.FinishIPSolutionEvent;
 import branchAndPrice.LexicographicMasterEvent;
+import branchAndPrice.RollbackEvent;
 import branchAndPrice.FinishLexicographicMasterEvent;
+import branchAndPrice.FinishRollbackEvent;
+import branchAndPrice.IPSolutionEvent;
 import columnGeneration.HeuristicMinCostLabelingPricingProblemSolver;
 import columnGeneration.HeuristicLabelingThirdPricingProblemSolver;
 import columnGeneration.HeuristicLabelingPricingProblemSolver;
@@ -318,6 +322,38 @@ public final class EVRPTWSolver {
 		public void CGProblemsLB(CGProblemsLBEvent cgProblemsLBEvent){
 			if (dataModel.print_log) {
 				logger.debug("Check problems with LB!");
+			}
+		}
+
+		public void IPRootNode(IPSolutionEvent IPRootEvent){
+			if (dataModel.print_log) {
+				logger.debug("=============== SOLVING IP ===============");
+			}
+		}
+
+		public void finishIPRootNode(FinishIPSolutionEvent IPRootEvent){
+			if (dataModel.print_log) {
+				logger.debug("Time solving the IP: "+getTimeInSeconds(IPRootEvent.time));
+			}
+		}
+
+		public void Rollback(RollbackEvent rollbackEvent){
+			if (dataModel.print_log){
+				logger.debug("=============== SRCs ROLLBACK ===============");
+				logger.debug("Performing a rollback step due to a label explosion in the Pricing Problem.");
+				logger.debug("Baseline: " + rollbackEvent.BL + ". Generated: " + rollbackEvent.explosion);
+				logger.debug("Returning to the previous optimal RMP state before the last generation of cuts.");
+			}
+		}
+
+		public void finishRollback(FinishRollbackEvent rollbackEvent){
+			if (dataModel.print_log){
+				logger.debug("Objective: "+ rollbackEvent.objective);
+				logger.debug("Number of columns: " + rollbackEvent.ncols + " Number of SRC separated: " + rollbackEvent.nSRCs);
+				logger.debug("Number of vehicle branches: " + rollbackEvent.nVehicleBranches);
+				logger.debug("Columns (only non-zero columns are returned):");
+				for(Route route: rollbackEvent.solution)
+					logger.debug(route.toString());
 			}
 		}
 
