b65bf006b86bcf37b26662b9fffce534f73418bd
Ariel Rojas <ari_rojasa@hotmail.com>
2026-05-28 10:38:21 +0200

    name

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -227,8 +227,8 @@ public final class EVRPTWSolver {
 
 		int gamma = Integer.parseInt(args[1]);
 
-		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "TSL-IP-CS-Bound", args[2]);
-		//EVRPTW evrptw = new EVRPTW("C108-50", 1, 0, true, "TwoLabeling-Bound", "Debug");
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "TSL-5GR35-RB100", args[2]);
+		//EVRPTW evrptw = new EVRPTW("R106-25", 3, 0, true, "TSL-5GR35-RB100", "Debug");
 		EVRPTWSolver Solver =  new EVRPTWSolver(evrptw);
 
 		Solver.solve(10800000L); evrptw.fileOut.close();
