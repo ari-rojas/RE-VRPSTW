99c03294977eb6e960ae11a0986dbde4826ae990
Ariel Rojas <ari_rojasa@hotmail.com>
2026-08-04 16:39:45 +0200

    Creo que ya

--- src/model/EVRPTWSolver.java
+++ src/model/EVRPTWSolver.java
@@ -258,12 +258,12 @@ public final class EVRPTWSolver {
 	 * */
 	public static void main(String[] args) throws IOException{
 
-		//int gamma = Integer.parseInt(args[1]);
-		//String energy_deviation = args[3];
-		//if (!energy_deviation.equals("")) energy_deviation = "-"+energy_deviation;
+		int gamma = Integer.parseInt(args[1]);
+		String energy_deviation = args[3];
+		if (!energy_deviation.equals("")) energy_deviation = "-"+energy_deviation;
 
-		//EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "FRC-1GR25-RB50"+energy_deviation, args[2], args[3]);
-		EVRPTW evrptw = new EVRPTW("RC105-50", 5, 0, true, "NF", "Debug", "");
+		EVRPTW evrptw = new EVRPTW(args[0], gamma, 0, true, "FRC-1GR25-RB50"+energy_deviation, args[2], args[3]);
+		//EVRPTW evrptw = new EVRPTW("RC202-50", 5, 0, true, "NF", "Debug", "");
 
 		EVRPTWSolver Solver = new EVRPTWSolver(evrptw, new ArrayList<>());
 
